2701590ac7d29b1f079456079119e516ad59a059
inorichi <len@kanade.eu>
2023-12-28 16:44:59 +0100

    Increase report timeout

--- src/pmw3610.c
+++ src/pmw3610.c
@@ -645,7 +645,7 @@ static int pmw3610_report_data(const struct device *dev) {
 
 #ifdef CONFIG_PMW3610_POLLING_RATE_125_SW
     int64_t curr_time = k_uptime_get();
-    if (data->last_poll_time == 0 || curr_time - data->last_poll_time > 8) {
+    if (data->last_poll_time == 0 || curr_time - data->last_poll_time > 128) {
         data->last_poll_time = curr_time;
         data->last_x = x;
         data->last_y = y;
